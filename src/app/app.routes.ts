90978a5f9af480de29c6cd9531a916367b059a93
ZouariOmar <zouariomar20@gmail.com>
2026-01-20 13:45:52 +0100

    feat(services-page): Add `Services` page
    [DETAILS]
    - A src/app/pages/services/*
    - M src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
   { path: 'about', loadComponent: () => import('./pages/about/about').then(m => m.About) },
-  // { path: 'services', loadComponent: () => import('./pages/services/services').then(m => m.Services) },
+  { path: 'services', loadComponent: () => import('./pages/services/services').then(m => m.Services) },
   // { path: 'sectors', loadComponent: () => import('./pages/sectors/sectors').then(m => m.Sectors) },
   // { path: 'operational-methodology', loadComponent: () => import('./pages/ops/ops').then(m => m.Ops) },
   // { path: 'use-cases', loadComponent: () => import('./pages/cases/cases').then(m => m.Cases) },
---
+++ src/app/pages/services/services.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../../core/components/navbar/navbar';
+import { Footer } from '../../../core/components/footer/footer';
+import WebsiteInfo from '../../models/website-info';
+
+@Component({
+  selector: 'app-services',
+  imports: [Navbar, Footer],
+  templateUrl: './services.html',
+  styleUrl: './services.css',
+})
+
+export class Services {
+  public readonly website!: WebsiteInfo;
+
+  constructor() {
+    this.website = new WebsiteInfo(
+      "Lebo",
+      "Labs"
+    )
+  }
+} // Services class
