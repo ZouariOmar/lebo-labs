9bd7144bd6b1ada69e49707201ff5a2372aa9562
ZouariOmar <zouariomar20@gmail.com>
2026-01-20 17:06:45 +0100

    feat(ops-page): Add `Operational Methodology` page
    [DETAILS]
    - A src/app/pages/ops/*
    - M src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
   { path: 'about', loadComponent: () => import('./pages/about/about').then(m => m.About) },
   { path: 'services', loadComponent: () => import('./pages/services/services').then(m => m.Services) },
   { path: 'sectors', loadComponent: () => import('./pages/sectors/sectors').then(m => m.Sectors) },
-  // { path: 'operational-methodology', loadComponent: () => import('./pages/ops/ops').then(m => m.Ops) },
+  { path: 'ops-methodology', loadComponent: () => import('./pages/ops/ops').then(m => m.Ops) },
   // { path: 'use-cases', loadComponent: () => import('./pages/cases/cases').then(m => m.Cases) },
   // { path: 'blog', loadComponent: () => import('./pages/blog/blog').then(m => m.Blog) },
   // { path: 'contact', loadComponent: () => import('./pages/contact/contact').then(m => m.Contact) },
